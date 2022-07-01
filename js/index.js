e658e170f8edd4b0fac6e5034c38abc369c059c2
Agu Kingsley <agukingsley96@gmail.com>
2022-07-01 13:11:11 +0100

    init

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let cardQuestions = document.querySelectorAll(".faq-card__question");
+let cardAnswer = document.querySelectorAll(".faq-card__answer");
+
+cardQuestions.addEventListener("click", dropDown);
+
+function dropDown() {
+  console.log("Oh my, I'm working");
+}
